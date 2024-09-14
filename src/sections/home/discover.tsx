d371100acb83a882b533dae5daaf0ef67cc6ad5b
Ehtasham Al Noman <ehtasham@graduate.utm.my>
2024-09-14 17:55:20 +0800

    discover and value page

--- src/sections/home/discover.tsx
+++ src/sections/home/discover.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
 
 const Discover = () => {
   return (
-    <div>Unveiling Possibilities</div>
-  )
-}
+    <div className="bg-black h-[55vh] padding-y px-8 md:px-24 lg:px-28 xl:px-36 flex items-center justify-center mb-2">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="w-[90%] xl:w-[70%] text-center"
+      >
+        <span className="text-accent font-monoton font-[600] text-8xl md:text-5xl lg:text-6xl tracking-widest">
+          Unveiling Possibilities
+        </span>
+      </motion.div>
+    </div>
+  );
+};
 
-export default Discover
+export default Discover;
--- src/sections/home/values.jsx
+++ src/sections/home/values.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Values = () => {
   return (
-    <div>Values</div>
-  )
-}
+    <div className="w-full px-4 py-16 sm:px-8 md:px-16 lg:px-24 bg-white flex flex-col justify-center items-center">
+      {/* Heading Section */}
+      <div className="text-center mb-12">
+        <h1 className="text-black text-[48px] sm:text-[60px] lg:text-[86.33px] font-bold font-['Jura']">
+          Exception is the Standard
+        </h1>
+        <p className="text-black text-[24px] sm:text-[30px] lg:text-[37.71px] font-normal font-['Inter']">
+          Why choose Us
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Values
+export default Values;
