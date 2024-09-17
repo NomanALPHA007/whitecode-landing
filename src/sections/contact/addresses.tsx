fbe0c29efa83198cc26da0e33da48e06107cbafe
Ishmum <ishmum20000@gmail.com>
2024-09-17 16:54:53 +0800

    Update background

--- src/sections/contact/addresses.tsx
+++ src/sections/contact/addresses.tsx
@@ -27,7 +27,7 @@ const Addresses = () => {
 
   return (
     <div>
-      <div className="bg-black w-full">
+      <div className="bg-custom w-full">
         <div className="mx-auto max-w-[90%] w-auto overflow-hidden">
           <p className="text-[32px] sm:text-[48px] md:text-[56px] lg:text-[64px] mt-[100px] sm:mt-[150px] md:mt-[180px] lg:mt-[200px] font-montserrat text-white text-center whitespace-nowrap">
             Where to find Us
