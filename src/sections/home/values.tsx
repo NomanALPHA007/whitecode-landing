67a4f6f95f61a45548d8bd31daaab687be7c80eb
Ehtasham Al Noman <ehtasham@graduate.utm.my>
2024-09-21 19:54:49 +0800

    Value page update

--- src/sections/home/values.tsx
+++ src/sections/home/values.tsx
@@ -5,7 +5,7 @@ import { values } from "@/data";
 
 const Values = () => {
   return (
-    <div className="px-4 md:px-6 lg:px-12 py-12 md:py-24 lg:py-32 w-full flex flex-col items-center justify-center gap-10 md:gap-20">
+    <div className="px-4 md:px-6 lg:px-12 py-12 md:py-24 lg:py-32 w-full flex flex-col items-center justify-center gap-10 md:gap-20 overflow-hidden">
       {/* Main Title */}
       <motion.div
         initial={{ opacity: 0, x: 20 }}
@@ -31,7 +31,7 @@ const Values = () => {
         initial={{ opacity: 0, x: -20 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6 }}
-        className="w-full md:w-[80vw] lg:w-[70vw] flex justify-center items-center gap-4 lg:gap-8 flex-wrap"
+        className="w-[90vw] sm:w-[80vw] md:w-[70vw] lg:w-[60vw] flex justify-center items-center gap-2 sm:gap-4 md:gap-6 lg:gap-8 flex-wrap mx-auto"
       >
         <HomeValuesCarousel />
       </motion.div>
