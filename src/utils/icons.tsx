ccfd70e4c4a18d3028af6d28968587995e03b080
Ishmum <ishmum20000@gmail.com>
2024-09-24 14:17:15 +0800

    Add chat icon

--- src/utils/icons.tsx
+++ src/utils/icons.tsx
@@ -5,11 +5,13 @@ import {
 	FaYoutube,
 } from "react-icons/fa6";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { IoChatbubblesOutline } from "react-icons/io5";
 
 export const ICONS = {
 	instagram: <FaInstagram />,
 	twitter: <FaXTwitter />,
 	youtube: <FaYoutube />,
 	linkedin: <FaLinkedin />,
-	arrow_right: <FaLongArrowAltRight />
+	arrow_right: <FaLongArrowAltRight />,
+	chat: <IoChatbubblesOutline />,
 };
