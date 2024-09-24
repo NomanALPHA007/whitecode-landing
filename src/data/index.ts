19cc1483a24273fa3e369b3a1108470c9c5e488a
Ishmum <ishmum20000@gmail.com>
2024-09-24 17:36:52 +0800

    Remove hq from finland

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,74 +2,74 @@ import { ICONS } from "@/utils/icons";
 
 // Navbar Links
 export const navLinks = [
-  {
-    name: "Our Work",
-    link: "/work",
-  },
-  {
-    name: "Services",
-    link: "/services",
-  },
-  {
-    name: "About Us",
-    link: "/about",
-  },
-  // {
-  //   name: "News",
-  //   link: "/news",
-  // },
-  {
-    name: "Contact Us",
-    link: "/contact",
-  },
+	{
+		name: "Our Work",
+		link: "/work",
+	},
+	{
+		name: "Services",
+		link: "/services",
+	},
+	{
+		name: "About Us",
+		link: "/about",
+	},
+	// {
+	//   name: "News",
+	//   link: "/news",
+	// },
+	{
+		name: "Contact Us",
+		link: "/contact",
+	},
 ];
 
 // Footer Links
 export const footerLinks = {
-  menu: [
-    {
-      name: "Our Work",
-      link: "/work",
-    },
-    {
-      name: "Services",
-      link: "/services",
-    },
-    {
-      name: "About Us",
-      link: "/about",
-    },
-  ],
-  contact: [
-    // {
-    //   name: "News",
-    //   link: "/news",
-    // },
-    {
-      name: "Contact Us",
-      link: "/contact",
-    },
-  ],
+	menu: [
+		{
+			name: "Our Work",
+			link: "/work",
+		},
+		{
+			name: "Services",
+			link: "/services",
+		},
+		{
+			name: "About Us",
+			link: "/about",
+		},
+	],
+	contact: [
+		// {
+		//   name: "News",
+		//   link: "/news",
+		// },
+		{
+			name: "Contact Us",
+			link: "/contact",
+		},
+	],
 };
 
 // Footer Socials
 export const footerSocials = [
-  {
-    icon: ICONS.instagram,
-    link: "https://www.instagram.com",
-  },
-  {
-    icon: ICONS.twitter,
-    link: "https://www.twitter.com",
-  },
-  {
-    icon: ICONS.youtube,
-    link: "https://www.youtube.com",
-  },
-  {
-    icon: ICONS.linkedin,
-    link: "https://www.linkedin.com",
-  },
+	{
+		icon: ICONS.instagram,
+		link: "https://www.instagram.com",
+	},
+	{
+		icon: ICONS.twitter,
+		link: "https://www.twitter.com",
+	},
+	{
+		icon: ICONS.youtube,
+		link: "https://www.youtube.com",
+	},
+	{
+		icon: ICONS.linkedin,
+		link: "https://www.linkedin.com",
+	},
 ];
 
 export const carouselProjects = [
@@ -192,52 +192,52 @@ export const projects = [
 ];
 
 export const services = [
-  {
-    name: "Web Application",
-    img: "/images/projects/siteflow-p-2000.png",
-  },
-  {
-    name: "Mobile Application",
-    img: "/images/projects/siteflow-p-2000.png",
-  },
-  {
-    name: "Desktop Application",
-    img: "/images/projects/siteflow-p-2000.png",
-  },
-  {
-    name: "EAS",
-    img: "/images/projects/siteflow-p-2000.png",
-  },
-  {
-    name: "Custom AI & Image Processing Solution",
-    img: "/images/projects/siteflow-p-2000.png",
-  },
-  {
-    name: "Dedicated Software Team",
-    img: "/images/projects/siteflow-p-2000.png",
-  },
-  {
-    name: "IoT Application & Embedded Systems",
-    img: "/images/projects/siteflow-p-2000.png",
-  },
+	{
+		name: "Web Application",
+		img: "/images/projects/siteflow-p-2000.png",
+	},
+	{
+		name: "Mobile Application",
+		img: "/images/projects/siteflow-p-2000.png",
+	},
+	{
+		name: "Desktop Application",
+		img: "/images/projects/siteflow-p-2000.png",
+	},
+	{
+		name: "EAS",
+		img: "/images/projects/siteflow-p-2000.png",
+	},
+	{
+		name: "Custom AI & Image Processing Solution",
+		img: "/images/projects/siteflow-p-2000.png",
+	},
+	{
+		name: "Dedicated Software Team",
+		img: "/images/projects/siteflow-p-2000.png",
+	},
+	{
+		name: "IoT Application & Embedded Systems",
+		img: "/images/projects/siteflow-p-2000.png",
+	},
 ];
 
 export const values = [
-  {
-    title: "Empowering Women in Technology",
-    description:
-      "We are dedicated to promoting gender diversity and inclusion within the tech industry. Our initiatives focus on empowering women by providing opportunities, mentorship, and resources to excel in technology roles.",
-  },
-  {
-    title: "Supporting Employment for Individuals with Disabilities",
-    description:
-      "We are dedicated to promoting gender diversity and inclusion within the tech industry. Our initiatives focus on empowering women by providing opportunities, mentorship, and resources to excel in technology roles.",
-  },
-  {
-    title: "Advancing Tech Education in Underrepresented Areas",
-    description:
-      "We are dedicated to promoting gender diversity and inclusion within the tech industry. Our initiatives focus on empowering women by providing opportunities, mentorship, and resources to excel in technology roles.",
-  },
+	{
+		title: "Empowering Women in Technology",
+		description:
+			"We are dedicated to promoting gender diversity and inclusion within the tech industry. Our initiatives focus on empowering women by providing opportunities, mentorship, and resources to excel in technology roles.",
+	},
+	{
+		title: "Supporting Employment for Individuals with Disabilities",
+		description:
+			"We are dedicated to promoting gender diversity and inclusion within the tech industry. Our initiatives focus on empowering women by providing opportunities, mentorship, and resources to excel in technology roles.",
+	},
+	{
+		title: "Advancing Tech Education in Underrepresented Areas",
+		description:
+			"We are dedicated to promoting gender diversity and inclusion within the tech industry. Our initiatives focus on empowering women by providing opportunities, mentorship, and resources to excel in technology roles.",
+	},
 ];
 
 export const carouselNews = [
@@ -252,102 +252,105 @@ export const carouselNews = [
 ];
 
 export const allNews = [
-  {
-    name: "News 1",
-    img: "/images/projects/siteflow-p-2000.png",
-    link: "#",
-  },
-  {
-    name: "News 2",
-    img: "/images/projects/siteflow-p-2000.png",
-    link: "#",
-  },
-  {
-    name: "News 3",
-    img: "/images/projects/siteflow-p-2000.png",
-    link: "#",
-  },
-  {
-    name: "News 4",
-    img: "/images/projects/siteflow-p-2000.png",
-    link: "#",
-  },
-  {
-    name: "News 5",
-    img: "/images/projects/siteflow-p-2000.png",
-    link: "#",
-  },
-  {
-    name: "News 6",
-    img: "/images/projects/siteflow-p-2000.png",
-    link: "#",
-  },
-  {
-    name: "News 7",
-    img: "/images/projects/siteflow-p-2000.png",
-    link: "#",
-  },
-  {
-    name: "News 8",
-    img: "/images/projects/siteflow-p-2000.png",
-    link: "#",
-  },
+	{
+		name: "News 1",
+		img: "/images/projects/siteflow-p-2000.png",
+		link: "#",
+	},
+	{
+		name: "News 2",
+		img: "/images/projects/siteflow-p-2000.png",
+		link: "#",
+	},
+	{
+		name: "News 3",
+		img: "/images/projects/siteflow-p-2000.png",
+		link: "#",
+	},
+	{
+		name: "News 4",
+		img: "/images/projects/siteflow-p-2000.png",
+		link: "#",
+	},
+	{
+		name: "News 5",
+		img: "/images/projects/siteflow-p-2000.png",
+		link: "#",
+	},
+	{
+		name: "News 6",
+		img: "/images/projects/siteflow-p-2000.png",
+		link: "#",
+	},
+	{
+		name: "News 7",
+		img: "/images/projects/siteflow-p-2000.png",
+		link: "#",
+	},
+	{
+		name: "News 8",
+		img: "/images/projects/siteflow-p-2000.png",
+		link: "#",
+	},
 ];
 
 export const countries: any = {
-  Australia: {
-    name: "Australia",
-    address: "576 Flinders Lane Melbourne VIC 3000",
-    hq: "Global HQ",
-  },
-  Finland: { name: "Finland", address: "Helsinki, Finland 00100", hq: "Global HQ" },
-  Bangladesh: { name: "Bangladesh", address: "Oasis, House 47, Road 3A, Dhanmondi, Dhaka - 1205" },
+	Australia: {
+		name: "Australia",
+		address: "576 Flinders Lane Melbourne VIC 3000",
+		hq: "Global HQ",
+	},
+	Finland: { name: "Finland", address: "Helsinki, Finland 00100" },
+	Bangladesh: {
+		name: "Bangladesh",
+		address: "Oasis, House 47, Road 3A, Dhanmondi, Dhaka - 1205",
+	},
 };
 
 export const cardData = [
-  {
-    id: 1,
-    title: "Project White Code",
-    description:
-      "Delivering excellence through innovation in AI and software development.",
-    image:
-      "https://images.unsplash.com/photo-1501891037204-8754d498396b?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    rating: 5.0,
-  },
-  {
-    id: 2,
-    title: "Cutting-edge AI Solutions",
-    description:
-      "We specialize in delivering custom-tailored AI projects leveraging the latest advancements.",
-    image:
-      "https://images.unsplash.com/photo-1605810230434-7631ac76ec81?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    title: "Advanced Image Processing",
-    description:
-      "Our team excels in solving complex problems with cutting-edge image processing technologies.",
-    image:
-      "https://plus.unsplash.com/premium_photo-1725907643696-b30e86f66b28?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    title: "AI-driven Innovation",
-    description:
-      "We are dedicated to driving innovation and helping businesses thrive with AI-driven solutions.",
-    image:
-      "https://images.unsplash.com/photo-1642132652859-3ef5a1048fd1?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    rating: 4.7,
-  },
-  {
-    id: 5,
-    title: "Custom Software Development",
-    description:
-      "We provide custom software solutions to address unique business needs across various industries.",
-    image:
-      "https://plus.unsplash.com/premium_photo-1720287601920-ee8c503af775?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    rating: 4.9,
-  },
+	{
+		id: 1,
+		title: "Project White Code",
+		description:
+			"Delivering excellence through innovation in AI and software development.",
+		image:
+			"https://images.unsplash.com/photo-1501891037204-8754d498396b?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		rating: 5.0,
+	},
+	{
+		id: 2,
+		title: "Cutting-edge AI Solutions",
+		description:
+			"We specialize in delivering custom-tailored AI projects leveraging the latest advancements.",
+		image:
+			"https://images.unsplash.com/photo-1605810230434-7631ac76ec81?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		rating: 4.9,
+	},
+	{
+		id: 3,
+		title: "Advanced Image Processing",
+		description:
+			"Our team excels in solving complex problems with cutting-edge image processing technologies.",
+		image:
+			"https://plus.unsplash.com/premium_photo-1725907643696-b30e86f66b28?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		rating: 4.8,
+	},
+	{
+		id: 4,
+		title: "AI-driven Innovation",
+		description:
+			"We are dedicated to driving innovation and helping businesses thrive with AI-driven solutions.",
+		image:
+			"https://images.unsplash.com/photo-1642132652859-3ef5a1048fd1?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		rating: 4.7,
+	},
+	{
+		id: 5,
+		title: "Custom Software Development",
+		description:
+			"We provide custom software solutions to address unique business needs across various industries.",
+		image:
+			"https://plus.unsplash.com/premium_photo-1720287601920-ee8c503af775?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		rating: 4.9,
+	},
 ];
