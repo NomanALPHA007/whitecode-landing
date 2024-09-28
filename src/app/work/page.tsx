c63f0bfc05a39ee2dc12415d78672222bc0acbdc
Ishmum <ishmum20000@gmail.com>
2024-09-28 20:19:41 +0800

    Format code

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,17 +1,17 @@
-import { ImageCarousel } from '@/components'
-import { carouselProjects } from '@/data'
-import { AllWorks, Chat, WorkHero } from '@/sections'
-import React from 'react'
+import { ImageCarousel } from "@/components";
+import { carouselProjects } from "@/data";
+import { AllWorks, Chat, WorkHero } from "@/sections";
+import React from "react";
 
 const Work = () => {
-  return (
-    <main>
-      <WorkHero />
-      <ImageCarousel projects={carouselProjects} />
-      <AllWorks />
-      <Chat />
-    </main>
-  )
-}
+	return (
+		<main>
+			<WorkHero />
+			<ImageCarousel projects={carouselProjects} />
+			<AllWorks />
+			<Chat />
+		</main>
+	);
+};
 
-export default Work
+export default Work;
